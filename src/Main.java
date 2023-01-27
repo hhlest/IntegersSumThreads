94e4d101e0927cfaed0a94ee9c700224129ebe9c
Waminms <67516571+Waminms@users.noreply.github.com>
2023-01-27 11:18:42 +0200

    Array size changed to 200 000 000

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] numbers = RandomArray.create(200_000_000);
         Threads.createThreadsAndStartCalculation(numbers);
-        System.out.println(Arrays.toString(numbers));
     }
 }
